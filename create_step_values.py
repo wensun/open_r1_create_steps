8aad9f08933998eb4ca6ade3a0040ab7b3754cb8
wensun <ws455@cornell.edu>
2025-07-08 00:51:35 -0400

    .

--- create_step_values.py
+++ create_step_values.py
@@ -152,7 +152,10 @@ if __name__ == "__main__":
     print("#######################")
     print(len(parsed_data))
     print(len(parsed_data["prompt_len"]))    
-
+    
+    HF_dataset = Dataset.from_dict(parsed_data)
+    HF_dataset.push_to_hub("wen-sun/openr1_token_wise_values")
+    
     #if accelerator.is_main_process:
     #    HF_dataset = Dataset.from_dict(parsed_data)
     #    HF_dataset.push_to_hub("wen-sun/openr1_token_wise_values")
