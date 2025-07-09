e10bce45eae06c579d62ce55f26b71d20d32ae44
wensun <ws455@cornell.edu>
2025-07-08 22:28:38 -0400

    .

--- create_step_values.py
+++ create_step_values.py
@@ -157,9 +157,9 @@ def generate_values(
         #### print(f"Process {accelerator.process_index} handling IDs: {batch['id'].tolist()}")
         new_data['prompt_len'] += batch['prompts_len'].tolist()
         #new_data['prompt_generation_tokenized']+= batch['tokenized_inputs'].tolist()
-        new_data['prompt_generation_tokenized'] += [row for row in batch['tokenized_inputs']]
+        new_data['prompt_generation_tokenized'] += [row.tolist() for row in batch['tokenized_inputs']]
         #new_data['success_probs'] += outputs['success_probs'].tolist()
-        new_data['success_probs'] += [row for row in outputs['success_probs']] 
+        new_data['success_probs'] += [row.tolist() for row in outputs['success_probs']] 
         new_data['rewards'] += batch['rewards'].tolist()
     
     return new_data
